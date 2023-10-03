3c81ed38ecb899dc3514514d22ed5dba4026abce
YesSyedAbdullah <146854868+YesSyedAbdullah@users.noreply.github.com>
2023-10-03 17:04:06 +0200

    Create 01.Beginning.R

---
+++ 01.Beginning.R
@@ -0,0 +1,18 @@
+# Here I can write anything I want! 42 is the meaning of life univcerse and all!
+
+# R as calculator
+2+3
+
+# Assign to an object
+Abdullah <- 2 + 3
+Abdullah
+
+Ali <- 5+3
+Ali
+
+Abdullah* Ali
+
+Final <- Abdullah * Ali
+Final
+
+Final^2
