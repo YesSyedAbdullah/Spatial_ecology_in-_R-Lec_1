fb46e18c99bfece5162b372749f2f3556cb29559
YesSyedAbdullah <146854868+YesSyedAbdullah@users.noreply.github.com>
2023-10-10 17:43:15 +0200

    Update 02.Population.R

--- 02.Population.R
+++ 02.Population.R
@@ -27,6 +27,7 @@ plot(bei, cex=0.2, pch=19)
 bei.extra
 plot(bei.extra)
 
+
 #let's use only part of dataset: elev
 plot(bei.extra$elev)
 
@@ -40,12 +41,52 @@ plot(elevation)
 elevation2 <- bei.extra[[1]]
 plot(elevation2)
 
+.
+.
+.
 
+##Lecture 04:
+# Passing from points to a continuous surface
+densitymap <- density(bei)
 
+##colorRampPalette: for changing color
+#100 is the gap (passing from 1 color to other)
+colorRampPalette(c("black", "red", "orange", "yellow")) (100)
+cl <- colorRampPalette(c("black", "red", "orange", "yellow")) (100)
+plot (densitymap, col=cl)
 
+#Don't use capital letter in R
 
+cl2 <- colorRampPalette(c("oldlace", "rosybrown1", "tan2", "sienna")) (100)
+plot (densitymap, col=cl2)
+#
+#Additional or New data set
 
+plot(bei.extra)
+#select only elevation not gradient 
+f#for select elev we have two options
+elev <- bei.extra [[1]] #bei.extra$elev
+plot(elev)
 
+#how to make multiframe:
+In mf we have 2 boxes in graph left is density and right is elevation (in 1 row and 2 column)
+#let's practice:
+par(mfrow=1, c(1,2))
+plot(densitymap)
+plot(elev)
+#so left m density map of right m elev ajayega
+#but if you want upr density map aye or nichy elev so u have to do..
+par(mfrow=c(2,1))
+plot(densitymap)
+plot(elev)
+
+#assignment make 3 ggraph 
+#1 with plot (bei) #2nd densitymap #3rd elevation
+
+par(mfrow=c(1,3))
+plot(bei)
+plot(densitymap)
+plot(elev)
 
 
 
