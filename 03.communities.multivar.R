165483ea6e01aa339db3a07321d6a716257c5774
Syed Abdullah Shah <146854868+YesSyedAbdullah@users.noreply.github.com>
2023-10-19 17:12:16 +0200

    Update 03.communities.multivar.R

--- 03.communities.multivar.R
+++ 03.communities.multivar.R
@@ -1,1 +1,48 @@
+#lecture 07
+
+library(vegan)
+data(dune)
+head(dune)
+
+### DECORANA is a program for aligning multivariate species data, performing correspondence analysis and detrended correspondence analysis (DCA)
+##simple (taking data and align in simple way)
+
+ord <- decorana(dune)
+ldc1 = 3.7004 
+ldc2 = 3.1166
+ldc3 = 1.30055 
+ldc4 = 1.47888
+
+total = ldc1 + ldc2 + ldc3 + ldc4
+
+pldc1 = ldc1 * 100/ total
+pldc2 = ldc2 * 100/ total
+pldc3 = ldc3 * 100/ total
+pldc4 = ldc4 * 100/ total
+
+pldc1
+pldc2
+
+plot(ord)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
