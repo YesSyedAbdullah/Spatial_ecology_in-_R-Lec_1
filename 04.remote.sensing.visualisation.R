a41a6c143445e1f3503869db6118c0700e8c4d25
Syed Abdullah Shah <146854868+YesSyedAbdullah@users.noreply.github.com>
2023-11-02 17:17:34 +0100

    Update 04.remote.sensing.visualisation.R

--- 04.remote.sensing.visualisation.R
+++ 04.remote.sensing.visualisation.R
@@ -33,6 +33,47 @@ plot(b3, col-cl)
 cl <- colorRampPalette(c("darkgrey", "grey", "lightgrey")) (100)
 plot(b3, col=cl)
 
+#####################################################################################################################################################################
+##02/11/2023
+library(imageRy)
+library(terra)
+
+im.list()
+im.import("sentinel.dolomites.b2.tif")
+
+cl<- colorRampPalette(c("black", "grey", "lightgrey")) (100)
+plot(b2, col=cl)
+#import the blue band from sentinel-2 (band 2 b2)
+im.import("sentinel.dolomites.b2.tif")
+plot(b2, col=cl)
+
+#import the green band from sentinel-2 (band 3 b3)
+b3<- im.import("sentinel.dolomites.b3.tif")
+plot(b3, col=cl)
+
+#import the red band from sentinel-2 (band 4)
+b4<- im.import("sentinel.dolomites.b4.tif")
+plot(b4, col=cl)
+
+#import the NIR band from sentinel-2 (band 8)
+b8<- im.import("sentinel.dolomites.b8.tif")
+plot(b8, col=cl)
+
+#multiframe sb pic ko sath la kr compare krengy. several graphs all together
+par(mfrow=c(2,2))
+plot(b2, col=cl)
+plot(b3, col=cl)
+plot(b4, col=cl)
+plot(b8, col=cl)
+
+#charon bands alg alg hain unko ek package m pack kr rhy ab #several layer all together in a one pack
+#stack images
+stacksent <- c(b2, b3, b4, b8)
+dev.off() #it closes devices
+plot(stacksent, col=cl)
+
+plot(stacksent[[4]], col=cl)
+
 
 
 
