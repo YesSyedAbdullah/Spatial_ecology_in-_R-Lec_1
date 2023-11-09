113536bc6f0af1163f5b28a3e2fbc4462dcdd8ad
Syed Abdullah Shah <146854868+YesSyedAbdullah@users.noreply.github.com>
2023-11-09 17:44:23 +0100

    Update 05.spectral.indices.R

--- 05.spectral.indices.R
+++ 05.spectral.indices.R
@@ -56,7 +56,49 @@ im.plotRGB(m2006, 2, 1, 3)
 
 # Exercise: plot the 2006 image with NIR in the Blue component of the RGB space
 im.plotRGB(m2006, 2, 3, 1)
+###################################################################################################################
+09-11-2023
+###################################################################################################################
+
+#exercise: build multiframe with 1992 and 2006
+par(mfrow=c(1,2)
+im.plotRGB(m1992, r=2, g=3, b=1)
+im.plotRGB(m2006, r=2, g=3, b=1)
+
+#DVI =NIR- RED
+#bands: 1=NIR, 2=RED, 3=Green
+dvi1992= m1992[[1]]-m1992[[2]]
+plot(dvi1992)
+
+cl<- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi1992, col=cl)
+
+#exercise: calculate the dvi of 2006
+dvi2006=m2006[[1]]-m2006[[2]]
+plot(dvi2006)
+
+cl<- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi2006, col=cl)
+
+#NDVI for 1992
+ndvi1992= (m1992[[1]] - m1992[[2]]) / (m1992[[1]] + m1992[[2]])  
+ndvi1992= dvi1992/ (m1992[[1]] + m1992[[2]])   
+plot(ndvi1992, col=cl)
+
+
+#NDVI for 2006
+ndvi2006= (m2006[[1]] - m2006[[2]]) / (m2006[[1]] + m2006[[2]])  
+ndvi2006= dvi2006/ (m2006[[1]] + m2006[[2]])   
+plot(ndvi2006, col=cl)
+
+par(mfrow=c(1,2))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
+
+#speeding up calculation
+ndvi2006a <- im.ndvi(m2006, 1, 2)
+plot(ndvi2006a, col=cl)
 
 
 
-
+    
