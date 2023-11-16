37e4ffd8b6790ab5909aa7abf62326a6815ddeec
Syed Abdullah Shah <146854868+YesSyedAbdullah@users.noreply.github.com>
2023-11-16 16:48:33 +0100

    Create 07.external.data.code.import.R

---
+++ 07.external.data.code.import.R
@@ -0,0 +1,8 @@
+#External data
+#sb sy phly R ko btana pry ga k wo data jsko import krna h R m wo kaha pr h
+
+#set working directory #based on my work directory
+#for window users: c:\\ users\path etc. -> c:// users/path etc.
+setwd("C:/Users/utente/Documents/GCE & SDGs/Lectures/Spatial Ecology in R")
+or
+setwd("-/Spatial Ecology in R") #hm koi symbol use kr skty pury path k jaga (jesy prof. ny kia) but not in windows
