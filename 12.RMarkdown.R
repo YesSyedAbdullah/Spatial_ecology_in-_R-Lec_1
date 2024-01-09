ebdd657ecc0bdb284e34c30e03e81363dd20a924
Syed Abdullah Shah <146854868+YesSyedAbdullah@users.noreply.github.com>
2024-01-09 17:40:43 +0100

    Create 12.RMarkdown.R

---
+++ 12.RMarkdown.R
@@ -0,0 +1,21 @@
+---
+title: "MarkdownReport"
+author: "Syed Abdullah"
+date: "2024-01-09"
+output: html_document
+---
+
+#My first Markdown document!
+
+Here we can add some text if we want!
+
+#``` copy these commas b/c in github it is not available. It is very important because and after of the codes just like open and end.
+#because of eval=T we will get attain results
+```{r, eval=T}
+2+3
+```
+
+#eval=T, so it will give answer, but if we add False so it will not give answer
+```{r, eval=F}
+2+3
+```
