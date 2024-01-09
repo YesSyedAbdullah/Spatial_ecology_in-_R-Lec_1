bd0bba5271150c1dd6f760bf95c179b75464ba04
Syed Abdullah Shah <146854868+YesSyedAbdullah@users.noreply.github.com>
2024-01-09 16:51:44 +0100

    Update 00.Final.Script.R

--- 00.Final.Script.R
+++ 00.Final.Script.R
@@ -6,7 +6,14 @@
 #02.1 Population Density
 #02.2 Poplation Distribuition
 #03.1 Communities Multivariate Analysis 
-#03.2 Communities Overlap
+#03.2 Communities Overlap 
+#04. Remote Sensing Visualisation
+#05. Spectral Indices
+#06. Times Series
+#07. External Data
+#08.Copernicus Data temp
+#09.
+#10.
 #------------------------------------------------------------------
 #01 Beginning
 # Here I can write anything I want! 42 is the meaning of life univcerse and all!
@@ -269,9 +276,369 @@ pldc2
 
 plot(ord)
 #------------------------------------------------------------------
+#03.2 Communities Overlap
+
+#relation among species in time
+
+#how different species overlap each other
+
+library(overlap)
+
+# data
+data(kerinci)
+head(kerinci)
+summary(kerinci)
+
+# selecting the first species
+tiger <- kerinci[kerinci$Sps=="tiger",]
+tiger <- kerinci[kerinci$Sps=="tiger",]
+
+# tiger
+# The unit of time is the day, so values range from 0 to 1. 
+# The package overlap works entirely in radians: fitting density curves uses trigonometric functions (sin, cos, tan),
+# so this speeds up simulations. The conversion is straightforward: kerinci$Time * 2 * pi
+kerinci$timeRad <- kerinci$Time * 2 * pi
+
+kerinci$timeRad <- kerinci$Time *2 * pi
+
+# selecting the first species
+tiger <- kerinci[kerinci$Sps=="tiger",]
+
+plot(tiger$timeRad)
+
+# selecting the time for the tiger
+timetig <- tiger$timeRad
+densityPlot(timetig, rug=TRUE)
+
+##exercise: select only data of monkey (macaque) individuals
+# selecting the second species macaque
+macaque <- kerinci[kerinci$Sps=="macaque",]
+timemac <- macaque$timeRad
+densityPlot(timemac, rug =TRUE)
+
+#Overlap.......
+overlapPlot (timetig, timemac)
+#------------------------------------------------------------------
+#04. Remote Sensing Visualisation
+
+##### BOOKS#########R coding  for ecology###### all codes####
+# This is a script to visualize satellite data
+
+library(devtools) # packages in R are also called libraries
+
+# install the imageRy package from GitHub
+devtools::install_github("ducciorocchini/imageRy") #this imageRy package is available in github not in CRAN.
+
+library(imageRy)
+library(terra) #terra pkg is used by imageRy pkg thats why we used it.
+
+im.list()
+sentinel.dolomites are satelites
+
+b2<- im.import("sentinel.dolomites.b8.tiff")
+
+b2 <- im.import("sentinel.dolomites.b2.tif") # b2 is the blue wavelength
+b2
+
+#######
+
+clb<- colorRampPalette(c("dark grey", "grey", "light grey")) (100)
 
 
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100) # 100 is the amount of colours in the gradient
+plot(b2, col=clb)
 
+#import the green band from sentinel-2 (band 3)
+#
+b3 <- im.import("sentinel.dolomites.b3.tif")
+plot(b3, col-cl)
+
+cl <- colorRampPalette(c("darkgrey", "grey", "lightgrey")) (100)
+plot(b3, col=cl)
+
+#####################################################################################################################################################################
+##02/11/2023
+library(imageRy)
+library(terra)
+
+im.list()
+im.import("sentinel.dolomites.b2.tif")
+
+cl<- colorRampPalette(c("black", "grey", "lightgrey")) (100)
+plot(b2, col=cl)
+#import the blue band from sentinel-2 (band 2 b2)
+im.import("sentinel.dolomites.b2.tif")
+plot(b2, col=cl)
+
+#import the green band from sentinel-2 (band 3 b3)
+b3<- im.import("sentinel.dolomites.b3.tif")
+plot(b3, col=cl)
+
+#import the red band from sentinel-2 (band 4)
+b4<- im.import("sentinel.dolomites.b4.tif")
+plot(b4, col=cl)
+
+#import the NIR band from sentinel-2 (band 8)
+b8<- im.import("sentinel.dolomites.b8.tif")
+plot(b8, col=cl)
+
+#multiframe sb pic ko sath la kr compare krengy. several graphs all together in  a single window
+par(mfrow=c(2,2))
+plot(b2, col=cl)
+plot(b3, col=cl)
+plot(b4, col=cl)
+plot(b8, col=cl)
+
+#charon bands alg alg hain unko ek package m pack kr rhy ab #several layer all together in a one pack
+#stack images
+stacksent <- c(b2, b3, b4, b8)
+dev.off() #it closes devices
+plot(stacksent, col=cl)
+
+plot(stacksent[[4]], col=cl)
+
+##Exercise : plot in a multiframe the bands with different color ramps##############
+par(mfrow=c(2,2)
+clb <- colorRampPalette(c("darkblue", "blue", "lightblue")) (100)
+plot(b2, col=clb)
+
+clg <- colorRampPalette(c("darkgreen", "green", "lightgreen")) (100)
+plot(b3, col=clg)
+    
+clr <- colorRampPalette(c("darkred", "red", "pink")) (100)
+plot(b4, col=clr)
+
+cln <- colorRampPalette(c("brown", "orange", "yellow")) (100)
+plot(b8, col=cln)
+
+par(mfrow=c(2,2))
+plot(b2, col=clb)
+plot(b3, col=clg)
+plot(b4, col=clr)
+plot(b8, col=cln)
+
+
+#im.plotRGB: plotRGB choosing
+
+#RGB space
+#stacksent: 
+#band2 blue element 1, stacksent[[1]]
+#band3 green element 2,  stacksent[[2]]
+#band4 red element 3,  stacksent[[3]]
+#band 8 nir element 4,  stacksent[[4]]
+
+im.plotRGB(stacksent, r=3, g=2, b=1)
+#------------------------------------------------------------------
+#05. Spectral Indices
+#har bar jb jb R open krogy tb tb library krna pryga
+
+library(imageRy)
+library(terra)
+#import all bands in R 
+im.list()
+
+b2<- im.import("sentinel.dolomites.b2.tif")
+b3<- im.import("sentinel.dolomites.b3.tif")
+b4<- im.import("sentinel.dolomites.b4.tif")
+b8<- im.import("sentinel.dolomites.b8.tif")
+
+stacksent <- c(b2, b3, b4, b8)
+plot(stacksent)
+
+im.plotRGB(stacksent, r=3, g=2, b=1)
+### 1, 2, 3, 4 bands ki sequences hain..
+##red=3, green=2, blue=1, NIR=4
+
+#######ksi 3 colors ko use kr skty tou ab NIR color ko layengy blue k jagay.. blue ko bye bye kro
+im.plotRGB(stacksent, r=4, g=3, b=2)
+
+##NIR k position ko change kr skty hain
+im.plotRGB(stacksent, r=3, g=4, b=2)
+im.plotRGB(stacksent, r=3, g=2, b=4)
+
+###############################################SOME presentation and graphs in register
+#######################################################################################
+then,
+install.packages("ggplot2")
+install.packages("viridis")
+
+library(ggplot2)
+library(viridis)
+
+#import new bands
+im.list()
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
+
+im.plotRGB(m1992, r=1, g=2, b=3)
+####or 
+im.plotRGB(m1992, 1,2,3) #both are same
+
+#### now change the positions
+im.plotRGB(m1992, r=2, g=1, b=3)
+im.plotRGB(m1992, r=2, g=3, b=1)
+
+#import the new/ recent images (from 1992 to 2006)
+# import the 2006 image
+m2006 <- im.import("matogrosso_ast_2006209_lrg")
+
+im.plotRGB(m2006, r=2, g=3, b=1)
+
+# Exercise: plot the 2006 image with NIR in the Green component of the RGB space
+im.plotRGB(m2006, 2, 1, 3)
+
+# Exercise: plot the 2006 image with NIR in the Blue component of the RGB space
+im.plotRGB(m2006, 2, 3, 1)
+###################################################################################################################
+09-11-2023
+###################################################################################################################
+
+#exercise: build multiframe with 1992 and 2006
+par(mfrow=c(1,2)
+im.plotRGB(m1992, r=2, g=3, b=1)
+im.plotRGB(m2006, r=2, g=3, b=1)
+
+#DVI =NIR- RED
+#bands: 1=NIR, 2=RED, 3=Green
+dvi1992= m1992[[1]]-m1992[[2]]
+plot(dvi1992)
+
+cl<- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi1992, col=cl)
+
+#exercise: calculate the dvi of 2006
+dvi2006=m2006[[1]]-m2006[[2]]
+plot(dvi2006)
+
+cl<- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi2006, col=cl)
+
+#NDVI for 1992
+ndvi1992= (m1992[[1]] - m1992[[2]]) / (m1992[[1]] + m1992[[2]])  
+ndvi1992= dvi1992/ (m1992[[1]] + m1992[[2]])   
+plot(ndvi1992, col=cl)
+
+
+#NDVI for 2006
+ndvi2006= (m2006[[1]] - m2006[[2]]) / (m2006[[1]] + m2006[[2]])  
+ndvi2006= dvi2006/ (m2006[[1]] + m2006[[2]])   
+plot(ndvi2006, col=cl)
+
+par(mfrow=c(1,2))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
+
+#speeding up calculation
+ndvi2006a <- im.ndvi(m2006, 1, 2)
+plot(ndvi2006a, col=cl)
+#------------------------------------------------------------------
+#06. Times Series
+
+#time series analysiss #before and after.
+library(imageRy)
+library(terra)
+
+im.list()
+
+#import the data
+EN01 <- im.import("EN_01.png")
+EN13 <- im.import("EN_13.png")
+
+par(mfrow=c(2,1))
+im.plotRGB.auto(EN01)
+im.plotRGB.auto(EN13)
+
+#difference b/w 2 images (jan and march)
+#using the first element 
+dif = EN01[[1]]-EN13[[1]]
+plot(dif)
+
+#pallette
+cldif <-colorRampPalette(c("blue", "white", "red")) (100)
+plot(dif, col=cldif)
+
+##New example: temperature in Greenland.
+g2000<- im.import("greenland.2000.tif")
+g2000
+clg <-colorRampPalette(c("blue", "white", "red")) (100)
+plot(g2000, cl=clg)
+
+g2005<- im.import("greenland.2005.tif")
+g2010<- im.import("greenland.2010.tif")
+g2015<- im.import("greenland.2015.tif")
+
+par(mfrow=c(2,1))
+plot(g2000, cl=clg)
+plot(g2015, cl=clg)
+
+#stacking the data (all four data together)
+stackg <- c(g2000, g2005, g2010, g2015)
+plot(stackg, col=clg)
+
+#exercise: make the differnece b/w the first and the final elements of the stack
+dif1 =g2000[[1]]-g2015[[1]]
+plot(dif1)
+
+or
+
+dif2<- stackg[[1]]-stackg[[4]]
+plot(dif2, col=cldif)
+
+#going to add RGB in 2000, 2005 and 2015,
+#2000 for red
+#2005 for green
+#2015 for blue 
+
+#if plot have high red tou temp zyda in 2000, if green clr is more so 2005 m temp zyda, or agr blue color zyda it means temp zyda hai 2015 m...
+#exercise: making RGB plot using different years.
+
+im.plotRGB(stackg, r=1, g=2, b=3)
+#------------------------------------------------------------------
+#07. External Data 
+
+#External data #to import the data from external part
+Library(terra)
+#sb sy phly R ko btana pry ga k wo data jsko import krna h R m wo kaha pr h
+
+#set working directory #based on my work directory
+#for window users: c:\\ users\path etc. -> c:// users/path etc.
+setwd("C:/Users/utente/Documents/GCE & SDGs/Lectures/Spatial Ecology in R")
+or
+setwd("-/Spatial Ecology in R") #hm koi symbol use kr skty pury path k jaga (jesy prof. ny kia) but not in windows only macbook m
+rast() #it is the exactly the same as im.import()
+naja<- rast("Najaf Iraq lake.jpg")
+plotRGB(naja, r=1, g=2, b=3) #Same as im.plotRGB
+
+#exercise download the second image from same website and put it inside R
+##No need to set working directory again
+##setwd("C:/Users/utente/Documents/GCE & SDGs/Lectures/Spatial Ecology in R")
+najaf2<- rast("Najaf Iraq lake 2023.jpg")
+plotRGB(najaf2, r=1, g=2, b=3)
+
+#two images together
+par(mfrow=c(2,1))
+plotRGB(naja, r=1, g=2, b=3) #Same as im.plotRGB
+plotRGB(najaf2, r=1, g=2, b=3)
+
+#exercise: multitemporal change detection
+najadif = naja[[1]] - najaf2[[2]]
+colorRampPalette(c("brown", "grey", "orange")) (100)
+plot(najadif, col=cl)
+
+#exercise: new pic import
+typhoon <- rast("ele.AMO2002250.jpg")
+plotRGB(typhoon, r=1, g=2, b=3)
+#now change the color/ bands
+plotRGB(typhoon, r=2, g=1, b=3)
+plotRGB(typhoon, r=3, g=2, b=1)
+
+#the matogrosso image can be download directly from the earth observatory website as well
+mato <- rast("matogrosso_l5_1992219.jpg")
+plotRGB(mato, r=1, g=2, b=3)
+#changing color
+plotRGB(mato, r=3, g=2, b=1)
+
+#cyrophere for ice data.#login kro phly 
+#------------------------------------------------------------------
 
 
 
